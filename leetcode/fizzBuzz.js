613a7a839358810eb19460d5b94261916653330c
shivansh6979 <shivansh11g@gmail.com>
2025-07-28 07:10:30 +0530

    Fizzbuzz problem

---
+++ leetcode/fizzBuzz.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  let temp = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      temp.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      temp.push("Fizz");
+    } else if (i % 5 == 0) {
+      temp.push("Buzz");
+    } else {
+      temp.push(`${i}`);
+    }
+  }
+  return temp;
+};
+
+console.log(fizzBuzz(15));
